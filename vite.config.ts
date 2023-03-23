938d06a7437def8d524f156e96f87c2b16ff66ef
garlandcrow <garlandcrow@proton.me>
2023-03-23 09:04:37 +0900

    add my lastest setup for node polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,27 @@ import { crx } from "@crxjs/vite-plugin";
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import manifest from "./manifest.json";
+import stdLibBrowser from "node-stdlib-browser";
+import inject from "@rollup/plugin-inject";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 export default defineConfig({
-  plugins: [svelte(), crx({ manifest })],
+  plugins: [
+    {
+      ...inject({
+        global: ["node-stdlib-browser/helpers/esbuild/shim", "global"],
+        process: ["node-stdlib-browser/helpers/esbuild/shim", "process"],
+        Buffer: ["node-stdlib-browser/helpers/esbuild/shim", "Buffer"],
+      }),
+      enforce: "post",
+    },
+    svelte(),
+    crx({ manifest }),
+  ],
   resolve: {
-    alias: {
-      process: "rollup-plugin-node-polyfills/polyfills/process-es6",
-    }
+    alias: stdLibBrowser,
   },
   optimizeDeps: {
-    esbuildOptions: {
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true,
-        }),
-      ],
-    },
+    include: ["buffer", "process"],
   },
 });
